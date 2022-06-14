5418cd6cca85647d875b65df8da8974ffb979388
sarah <skiyoko14@gmail.com>
2022-06-14 13:35:35 -0700

    more changes to use react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Can he TFT?</header>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-const getStats = () => {
-  axios
-    .get("http://127.0.0.1:5000/aaron")
-    .then((response) => {
-      console.log(response.data);
-      const gameCounter = document.getElementById("gameCounter");
-      const games = response.data;
-      console.log(games.length);
-      gameCounter.textContent = `${games.length}`;
-    })
-    .catch((response) => {
-      console.log("an error occurred");
-    });
-};
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+import App from "./App";
 
-const getMatches = () => {
-  axios
-    .get("http://127.0.0.1:5000/aaron/recent-matches")
-    .then((response) => {});
-};
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-document.addEventListener("DOMContentLoaded", getStats);
+// const getStats = () => {
+//   axios
+//     .get("http://127.0.0.1:5000/aaron")
+//     .then((response) => {
+//       console.log(response.data);
+//       const gameCounter = document.getElementById("gameCounter");
+//       const games = response.data;
+//       console.log(games.length);
+//       gameCounter.textContent = `${games.length}`;
+//     })
+//     .catch((response) => {
+//       console.log("an error occurred");
+//     });
+// };
+
+// const getMatches = () => {
+//   axios
+//     .get("http://127.0.0.1:5000/aaron/recent-matches")
+//     .then((response) => {});
+// };
+
+// document.addEventListener("DOMContentLoaded", getStats);
